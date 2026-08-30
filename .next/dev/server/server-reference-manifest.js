2729a2e31efdf7d4038608964a2d0744f4d91be5
CompsciiNikhil <compsciinikhil@gmail.com>
2026-08-30 23:35:09 +0530

    Stop tracking .next build output

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ygutPNVUcDPoB8r62STjt/GDUTU5r/FhSk00gQi610s=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_007e4b2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_globals_0yg4wg8.css","static/chunks/node_modules_1krnun1._.js","static/chunks/app_providers_tsx_10d2jbp._.js"],
-    source: "dynamic"
-});
